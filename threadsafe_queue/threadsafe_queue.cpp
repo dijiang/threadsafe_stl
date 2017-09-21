b15a6260ec0e37565a1277b8d2130fb7c09dcc34
liyf <1195878682@qq.com>
2017-09-21 21:34:36 +0800

    init

--- threadsafe_queue/threadsafe_queue.cpp
+++ threadsafe_queue/threadsafe_queue.cpp
@@ -8,44 +8,44 @@ template<typename T>
 class threadsafe_queue
 {
 public:
-    threadsafe_queue() {  };
-    //shared_ptr<T> try_pop();
-    //bool try_pop(T& value);
-    //shared_ptr<T> wait_and_pop();
-    //void wait_and_pop(T& value);
-    void push(T value);
-    //bool empty();
+	threadsafe_queue():_head(new node),_tail(_head.get()) { };
+	//shared_ptr<T> try_pop();
+	//bool try_pop(T& value);
+	//shared_ptr<T> wait_and_pop();
+	//void wait_and_pop(T& value);
+	void push(T value);
+	//bool empty();
 private:
-    struct node
-    {
-        shared_ptr<T> _data;
-        unique_ptr<T> _next;
-    };
-    unique_ptr<node> _head;
-    node* _tail;
-    mutex _headLock, _tailLock;
-    condition_variable _cv;
+	struct node
+	{
+		shared_ptr<T> _data;
+		unique_ptr<node> _next;
+	};
+	unique_ptr<node> _head;
+	node* _tail;
+	mutex _headLock, _tailLock;
+	condition_variable _cv;
 };
 
 template<typename T>
 void threadsafe_queue<T>::push(T value)
 {
-    shared_ptr<T> new_value = make_shared(std::move(value));
-    unique_lock<T> p(new node);
-    {
-        lock_guard(_tailLock);
-        node* new_tail = p.get();
-        _tail->_data = new_value;
-        _tail->_next = p;
-        _tail = new_tail;
-    }
-    _cv.notify_one();
+	shared_ptr<T> new_value = make_shared<T>(move(value));
+	unique_ptr<node> p(new node);
+	{
+		lock_guard<mutex> lock(_tailLock);
+		node* new_tail = p.get();
+		_tail->_data = new_value;
+		_tail->_next = move(p);
+		_tail = new_tail;
+	}
+	_cv.notify_one();
 }
 
 int main()
 {
-    threadsafe_queue<int> queue;
-    int i = 2;
-    queue.push(i);
-    return 0;
+	threadsafe_queue<int> queue;
+	int i = 2;
+	queue.push(i);
+	return 0;
 }
